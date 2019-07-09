3a0436de3ca767037fa4ca1499a337fbb311d335
huskyin <huskyin.com@gmail.com>
2019-07-09 15:14:19 +0700

    Update pertama kali

---
+++ src/main/java/id/catchatrade/Catcha/Trade/CatchaTradeApplication.java
@@ -0,0 +1,13 @@
+package id.catchatrade.Catcha.Trade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatchaTradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatchaTradeApplication.class, args);
+	}
+
+}
